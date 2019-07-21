68d046abe3646845a68c1898c4e8ed37f8782f4f
Kaiyu Shen <ks834@cornell.edu>
2019-07-20 18:44:57 -0700

    Monkey parser : add error printing

--- parser/parser.go
+++ parser/parser.go
@@ -4,6 +4,7 @@ import (
     "monkey/ast"
     "monkey/lexer"
     "monkey/token"
+    "fmt"
 )
 
 
@@ -14,15 +15,27 @@ type Parser struct {
     // is enough to hand code the recursive descent parser
     curToken  token.Token
     peekToken token.Token
+
+    errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-    p := &Parser{l:l}
+    p := &Parser{l:l, errors:[]string{}}
 
     p.nextToken()
     p.nextToken()
 
     return p
+}
+
+func (p *Parser) Errors() []string {
+    return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+    msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+                       t, p.peekToken.Type)
+    p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -93,6 +106,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
         p.nextToken()
         return true
     } else {
+        p.peekError(t)
         return false
     }
 }
